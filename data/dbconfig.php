a1dabb5115aef2f4621254a00b7b872da1b1185c
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-11-06 19:01:56 +0700

    fork : setup database tapi blm ku anu

---
+++ data/dbconfig.php
@@ -0,0 +1,17 @@
+<?php
+// Database configuration
+$databaseHost = 'LAPTOP-MHVH0S2R';
+$databaseName = ''; 
+$databaseUsername = '';
+$databasePassword = '';
+
+// Create database connection
+try {
+    $dsn = "sqlsrv:Server=$databaseHost;Database=$databaseName";
+    $pdo = new PDO($dsn, $databaseUsername, $databasePassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+?>
