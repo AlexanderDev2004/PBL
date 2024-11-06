b48b4be17fd8479c9132ba54f059aa781c700e74
Alexander Agung Raya <127664244+AlexanderDev2004@users.noreply.github.com>
2024-11-06 18:51:15 +0700

    del : hapus folder tidak guna dan upadte

--- MVC/app/Model/Student.php
+++
@@ -1,7 +0,0 @@
-class Student extends Model {
-    protected $table = 'students';
-
-    public function find($id) {
-        return $this->db->query("SELECT * FROM $this->table WHERE id = :id", ['id' => $id]);
-    }
-}
