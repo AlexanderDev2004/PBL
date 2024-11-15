b31d0fdc28b2857e8e94d46471baa693064125dc
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-11-15 09:35:26 +0700

    fork : controllerKomdis dan components di views

---
+++ controllers/controllersKomdis.php
@@ -0,0 +1,13 @@
+<!-- ini untuk controller komdis -->
+<?php
+
+include_once '../models/komdis.php';
+
+class controllersKomdis
+{
+    public function index()
+    {
+        include_once "views/komdis/index.php";
+    }
+}
+?>
