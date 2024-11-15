026e0dec6a391675c3fe0f4ba35d3c83d95f6072
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-11-15 09:39:36 +0700

    fotk : footer

---
+++ views/components/footer.php
@@ -0,0 +1,6 @@
+<!-- Footer di view -->
+<footer class="footer">
+    <div class="container">
+        <p>&copy; <script>document.write(new Date().getFullYear());</script> SI TT. All rights reserved.</p>
+    </div>
+</footer>
