689c5cea2254867edfb409a6ec39021bd758b9fc
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-11-18 20:32:09 +0700

    Fix doang saya bosan

--- controllers/controllersDosen.php
+++ controllers/controllersDosen.php
@@ -1,116 +1,24 @@
 <?php
+class ControllerDosen {
+    private $model;
 
-include_once '../models/tataTertib.php';
-
-class controllersTataTertib {
-    private $modelTataTertib;
-
-    public function __construct() {
-        $this->modelTataTertib = new TataTertib();
+    public function __construct($model) {
+        $this->model = $model;
     }
 
-    // Menampilkan semua data tata tertib
     public function index() {
-        $dataTataTertib = $this->modelTataTertib->getAllTataTertib();
-        include "views/tata_tertib/index.php";
+        $data = $this->model->getAllDosen();
+        include 'views/dosen/index.php';
     }
 
-    // Menampilkan form tambah tata tertib
-    public function create() {
-        include "views/tata_tertib/create.php";
+    public function detail($id) {
+        $data = $this->model->getDosenById($id);
+        include 'views/dosen/detail.php';
     }
 
-    // Menyimpan data tata tertib baru
-    public function store() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $data = [
-                'kode_tata_tertib' => $_POST['kode_tata_tertib'],
-                'kategori' => $_POST['kategori'],
-                'deskripsi' => $_POST['deskripsi'],
-                'sanksi' => $_POST['sanksi'],
-                'poin_pelanggaran' => $_POST['poin_pelanggaran'],
-                'status' => $_POST['status']
-            ];
-
-            $result = $this->modelTataTertib->tambahTataTertib($data);
-            if ($result) {
-                header("Location: index.php?page=tata_tertib&success=tambah");
-            } else {
-                header("Location: index.php?page=tata_tertib&error=tambah");
-            }
-        }
-    }
-
-    // Menampilkan form edit tata tertib
-    public function edit($id) {
-        $tataTertib = $this->modelTataTertib->getTataTertibById($id);
-        include "views/tata_tertib/edit.php";
-    }
-
-    // Update data tata tertib
-    public function update() {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $data = [
-                'id' => $_POST['id'],
-                'kode_tata_tertib' => $_POST['kode_tata_tertib'],
-                'kategori' => $_POST['kategori'],
-                'deskripsi' => $_POST['deskripsi'],
-                'sanksi' => $_POST['sanksi'],
-                'poin_pelanggaran' => $_POST['poin_pelanggaran'],
-                'status' => $_POST['status']
-            ];
-
-            $result = $this->modelTataTertib->updateTataTertib($data);
-            if ($result) {
-                header("Location: index.php?page=tata_tertib&success=update");
-            } else {
-                header("Location: index.php?page=tata_tertib&error=update");
-            }
-        }
-    }
-
-    // Hapus data tata tertib
-    public function delete($id) {
-        $result = $this->modelTataTertib->deleteTataTertib($id);
-        if ($result) {
-            header("Location: index.php?page=tata_tertib&success=hapus");
-        } else {
-            header("Location: index.php?page=tata_tertib&error=hapus");
-        }
-    }
-
-    // Mencari tata tertib
-    public function search() {
-        if (isset($_GET['keyword'])) {
-            $keyword = $_GET['keyword'];
-            $dataTataTertib = $this->modelTataTertib->searchTataTertib($keyword);
-            include "views/tata_tertib/index.php";
-        }
-    }
-
-    // Filter tata tertib berdasarkan kategori
-    public function filterByKategori($kategori) {
-        $dataTataTertib = $this->modelTataTertib->getTataTertibByKategori($kategori);
-        include "views/tata_tertib/index.php";
-    }
-
-    // Mendapatkan statistik pelanggaran
-    public function getStatistik() {
-        $statistik = $this->modelTataTertib->getStatistikPelanggaran();
-        include "views/tata_tertib/statistik.php";
-    }
-
-    // Export data tata tertib ke PDF
-    public function exportPDF() {
-        $dataTataTertib = $this->modelTataTertib->getAllTataTertib();
-        // Implementasi export ke PDF
-        // Bisa menggunakan library seperti FPDF atau TCPDF
-    }
-
-    // Export data tata tertib ke Excel
-    public function exportExcel() {
-        $dataTataTertib = $this->modelTataTertib->getAllTataTertib();
-        // Implementasi export ke Excel
-        // Bisa menggunakan library seperti PHPSpreadsheet
+    public function validatePelanggaran($pelanggaranId) {
+        $this->model->validatePelanggaran($pelanggaranId);
+        header('Location: ?controller=dosen&action=index');
     }
 }
+?>
--- controllers/controllersMahasiswa.php
+++ controllers/controllersMahasiswa.php
@@ -1,17 +1,24 @@
 <?php
-// controllersMahasiswa.php
+class ControllerMahasiswa {
+    private $model;
 
-session_start();
-require '../core/dbconfig.php'; // File koneksi database
-
-include_once '../models/user.php';
-include_once '../models/mahasiswa.php';
-
-class controllersMahasiswa
-{
-    public function index()
-    {
-        include "views/mahasiswa/index.php";
+    public function __construct($model) {
+        $this->model = $model;
     }
+
+    public function index() {
+        $data = $this->model->getStudentData($_SESSION['mahasiswa_id']);
+        include 'views/mahasiswa/index.php';
+    }
+
+    public function lihatPelanggaran() {
+        $data = $this->model->getPelanggaranByStudent($_SESSION['mahasiswa_id']);
+        include 'views/mahasiswa/pelanggaran.php';
+    }
+
+    // public function ajukanBanding($pelanggaranId, $alasan) {
+    //     $this->model->submitAppeal($pelanggaranId, $alasan);
+    //     header('Location: ?controller=mahasiswa&action=lihatPelanggaran');
+    // }
 }
 ?>
