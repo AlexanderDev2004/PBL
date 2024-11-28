c11d8054a1cfcab9a6464e25c59a8ba9342b87b1
om-ica <irsacahaya17@gmail.com>
2024-11-29 00:12:24 +0700

    Edit Header

--- views/mahasiswa.php
+++ views/mahasiswa.php
@@ -7,9 +7,41 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    <!-- Navbar -->
+    <header class="w-full max-w-screen-xl h-24 bg-[#132145] text-white flex flex-col items-center py-6 transform translate-x-64">
+        <div class="container mx-auto flex px-8 py-2 items-center justify-between">
+            <div class="flex items-center justify-between py-[-6] text-4xl font-bold text-primary ml-0"><span class="text-yellow-400">Reg</span>IT</div>
+        </div>
+
+        <!-- User Information -->
+        <div class="flex-col self-end space-x-3 px-8">
+        <!-- User Icon -->
+             <div class="bg-indigo-950 rounded-full p-1 my-[-4] relative mr-32">
+                 <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                 <g clip-path="url(#clip0_2_84)">
+                 <path d="M25 4.16667C13.5 4.16667 4.16667 13.5 4.16667 25C4.16667 36.5 13.5 45.8333 25 45.8333C36.5 45.8333 45.8333 36.5 45.8333 25C45.8333 13.5 36.5 4.16667 25 4.16667ZM25 10.4167C28.4583 10.4167 31.25 13.2083 31.25 16.6667C31.25 20.125 28.4583 22.9167 25 22.9167C21.5417 22.9167 18.75 20.125 18.75 16.6667C18.75 13.2083 21.5417 10.4167 25 10.4167ZM25 40C19.7917 40 15.1875 37.3333 12.5 33.2917C12.5625 29.1458 20.8333 26.875 25 26.875C29.1458 26.875 37.4375 29.1458 37.5 33.2917C34.8125 37.3333 30.2083 40 25 40Z" fill="white"/>
+                 </g>
+                 <defs>
+                 <clipPath id="clip0_2_84">
+                 <rect width="50" height="50" fill="white"/>
+                 </clipPath>
+                 </defs>
+                 </svg>
+             </div>
+      </div>
+      <!-- User Info Text -->
+      <div class = "flex-col self-end space-x-3 px-8">
+        <p class="text-black font-semibold">Mahasiswa 123</p>
+        <p class="text-black">29827166789</p>
+      </div>
+    </div>
+    </div>
+    </header>
+    <!-- Navbar End -->
+
     <!-- SideBar -->
     <div class="flex">
-        <div class="w-64 h-screen bg-[#132145] text-white flex flex-col items-center py-6">
+        <div class="w-64 h-screen bg-[#132145] text-white flex flex-col items-center py-6 absolute top-[-20px]">
             <div class="mb-6">
                 <div class="w-20 h-20 flex items-center justify-center rounded-lg mt-6">
                     <img src="../Public/LogoSide.png" alt="logo" class="w-20 h-20 object-contain">
