a0a63769d027cb1a695f361936179e37c111e966
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-11-29 08:30:06 +0700

    dari awal bang

--- views/components/footer.php
+++
@@ -1,8 +0,0 @@
-<!-- Footer di view -->
-<footer class="footer">
-    <div class="container">
-        <p>&copy; <script>document.write(new Date().getFullYear());</script> RegIT. All rights reserved.</p>
-    </div>
-</footer>
-
-
