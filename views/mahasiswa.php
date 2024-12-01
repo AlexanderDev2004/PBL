6c81ded2358169015edeaa4195d467923c25b054
pateh27 <fathzfl@gmail.com>
2024-12-01 18:07:20 +0700

    menambahkan halamann baru dan chartjs

--- views/mahasiswa.php
+++ views/mahasiswa.php
@@ -40,15 +40,16 @@
     <!-- Navbar End -->
 
     <!-- SideBar -->
-    <div class="flex">
+    <div class="flex ">
         <div class="w-64 h-screen bg-[#132145] text-white flex flex-col items-center py-6 absolute top-[-20px]">
             <div class="mb-6">
                 <div class="w-20 h-20 flex items-center justify-center rounded-lg mt-6">
                     <img src="../Public/LogoSide.png" alt="logo" class="w-20 h-20 object-contain">
                 </div>
             </div>
-            <nav class="w-full">
-                <a href="#" class="flex items-center gap-3 px-6 py-3 mb-2 bg-[#FEC01A] rounded-lg text-white rounded-lg text-sm font-bold hover:bg-yellow-600">
+            <nav class="w-full flex flex-col">
+                <a href="#" class="flex items-center gap-3 px-6 py-3 mb-2  rounded-lg text-white rounded-lg text-sm font-bold hover:bg-yellow-600"
+                aria-current="page">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="bg-white rounded-full">
                     <g clip-path="url(#clip0_6_8345)">
                     <path d="M11 15H13V17H11V15ZM11 7H13V13H11V7ZM11.99 2C6.47 2 2 6.48 2 12C2 17.52 6.47 22 11.99 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 11.99 2ZM12 20C7.58 20 4 16.42 4 12C4 7.58 7.58 4 12 4C16.42 4 20 7.58 20 12C20 16.42 16.42 20 12 20Z" fill="#323232"/>
