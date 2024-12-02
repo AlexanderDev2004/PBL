43cd11b752c8efaefb08521c96bf9b943b1b5143
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-12-03 00:50:41 +0700

    Mencoba_Mahasiswa

--- views/pages/dashboard/mahasiswa.php
+++ views/pages/dashboard/mahasiswa.php
@@ -24,7 +24,7 @@ if (sqlsrv_execute($stmt)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Mahasiswa</title>
-    <!-- Tambahkan CSS sesuai kebutuhan -->
+   
 </head>
 <body>
     <div class="container">
